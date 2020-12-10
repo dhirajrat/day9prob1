9fb0a3a77e0754190609341dd75d691efab3a326
dhirajrathod <dhirajrathod3000@gmail.com>
2020-12-10 17:57:29 +0530

    Adding Part time employee

--- employeeattendance.sh
+++ employeeattendance.sh
@@ -2,13 +2,21 @@
 
 WagePerHr=20
 FullDayHr=8
+PartTimeDayHr=6
+DayType=$((RANDOM%2))
 
 random=$((RANDOM%2))
 if (( $random == 0 ))
 then
 echo "employee is present"
-DailyWage=$(( $WagePerHr * $FullDayHr ))
-echo "Daily Salary is : $DailyWage"
+if (( $DayType == 0 ))
+then
+DailyPWage=$(( $WagePerHr * $PartTimeDayHr ))
+echo "Part Time Employee Daily Wage : $DailyPWage"
+else
+DailyFWage=$(( $WagePerHr * $FullDayHr ))
+echo "Full time Employee Daily Wage is : $DailyFWage"
+fi
 else
 echo "employee is absent"
 fi
