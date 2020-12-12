8bd3a2df8e9495f9d64dd1da6a2aea3310b8fa25
dhirajrathod <dhirajrathod3000@gmail.com>
2020-12-12 15:29:19 +0530

    monthly wages

--- employeeattendance.sh
+++ employeeattendance.sh
@@ -3,6 +3,8 @@
 WagePerHr=20
 FullDayHr=8
 PartTimeDayHr=6
+DaysOfMonth=20
+
 random=$((RANDOM%3))
 
 
@@ -12,10 +14,14 @@ case $random in
 	DailyPWage=$(( $WagePerHr * $PartTimeDayHr ))
 	echo "employee is present"
 	echo "Part Time Employee Daily Wage : $DailyPWage"
+	MonthlyPWage=$(( $DailyPWage * $DaysOfMonth ))
+	echo "Monthly Wage will be : $MonthlyPWage"
 	;;
 	1)
 	DailyFWage=$(( $WagePerHr * $FullDayHr ))
 	echo "Full time Employee Daily Wage is : $DailyFWage"
+	MonthlyFWage=$(( $DailyFWage * $DaysOfMonth ))
+	echo "Monthly Wage will be : $MonthlyFWage"
 	;;
 	2)
 	echo "employee is absent"
